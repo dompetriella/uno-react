04a0b712337dc605f7ec834ba7920b1535e33079
Dominic Petriella <dompetriella@gmail.com>
2021-10-22 18:44:50 -0400

    forgot to save index.js :s
    Co-authored-by: Dominic Petriella <dompetriella@gmail.com>
    Co-authored-by: Dustin Thurston <dthurston1996@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const io = new http.Server(server);
 
 app.get('/', (req, res) => {
       res.send('Hello world');
+});
+
+io.on('connection', (socket) => {
+    console.log('user connected');
 });
 
 server.listen(3000, () => {  
